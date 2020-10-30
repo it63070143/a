b087be192ccb8baab7760b4e4d25e7ab6c1da1f0
Yuttana Phongphuak <73682068+it63070143@users.noreply.github.com>
2020-10-30 12:59:12 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,27 @@
+<html>
+    <head>
+        <title>Welcome to Artanor</title>
+    <head>
+        <title>ROV</title>
+    <body>
+        <h1>Yuttana Phongphuak</h1>
+        <h2>Arm</h2>
+
+        <img src="allain.jpg">
+        <h3> allain: "If it's for you I don't care"</h3>
+
+        <img src="but.jpg">
+        <h3> Butterfly: "As long as you are there I can be strong"</h3>
+
+        <img src="X.jpg">
+        <h3> allain: "Even if I need to betray the whole world"</h3>
+        <h3> Butterfly: "Even if the world is full of darkness"</h3>
+
+        <h2>Webbb</h2>
+        <ul>
+            <li>Google</li>
+            <li>kapook</li>
+            <li>Amazon</li>
+        </ul>
+    </body>
+</head>
