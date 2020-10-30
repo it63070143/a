9faf1e55e314038c3fed0751147e0ed3d6c827b7
Yuttana Phongphuak <73682068+it63070143@users.noreply.github.com>
2020-10-30 13:08:43 +0700

    Update index.php

--- index.php
+++ index.php
@@ -16,7 +16,13 @@
         <img src="X.jpg">
         <h3> allain: "Even if I need to betray the whole world"</h3>
         <h3> Butterfly: "Even if the world is full of darkness"</h3>
+        <a href="https://onlearn.it.kmitl.ac.th/">a</a>
 
+
+        <h3>ลูก:พ่อครับบ้านเรามีแม่่บ้านหรอครับผมเห็นเมื่อวาน</h3>
+        <h3> พ่อ:ไปเก็บของเลย บ้านเราไม่มีแม่บ้าน</h3>
+        <h3> ลูก:ครับ</h3>
+        <h3>พ่อ:เอ้ะ แต่เราไม่เคยมีลูก</h3>
         <h2>Webbb</h2>
         <ul>
             <li>Google</li>
